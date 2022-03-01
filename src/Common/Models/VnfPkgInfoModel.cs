3299b2018fdbb12ee11f8532703d33f599e16bbf
Bartosz Bratuś <bartosz.bratus@study.beds.ac.uk>
2022-03-01 10:28:39 +0000

    Add Common project

---
+++ src/Common/Models/VnfPkgInfoModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Common;
+using System.Text.Json.Serialization;
+
+namespace Common.Models;
+
+public class VnfPkgInfoModel
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+}
