eb768f7505979c2884d38687a7a9771b225d078b
diwalipathak <diwali.diwali@beds.ac.uk>
2022-03-08 13:22:37 +0000

    created the request for action planner

---
+++ src/Common/Models/TaskPlannerInputModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Middleware.Common.Models
+{
+    public class TaskPlannerInputModel
+    {
+        [JsonPropertyName("RobotId")]
+        public Guid RobotId { get; set; }
+
+        [JsonPropertyName("TaskId")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("Questions")]
+        public List<DialogueModel> Questions { get; set; }
+
+
+    }
+}
---
+++ src/TaskPlanner/Controllers/PlanController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Middleware.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Middleware.TaskPlanner.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PlanController : ControllerBase
+    {
+       
+
+        [HttpPost] //http get request
+        [ProducesResponseType(typeof(TaskModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<TaskModel>> GetPlan([FromBody]TaskPlannerInputModel inputModel)
+        {
+            var plan =  new TaskModel();  
+            return Ok(plan);
+        }
+
+      
+    }
+}
