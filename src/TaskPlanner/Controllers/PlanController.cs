b6d16cf3c977b4ab0ba5462a032aa79cd59c1a8b
Bartosz Bratuś <bartosz.bratus@study.beds.ac.uk>
2022-03-08 16:19:05 +0000

    Revert "Update paths to build and obj folders for projects"
    This reverts commit 7b00dc706e89c9e74f06f94da8f2893f9359a8cd.

--- src/TaskPlanner/Controllers/PlanController.cs
+++ src/TaskPlanner/Controllers/PlanController.cs
@@ -2,12 +2,15 @@
 using Middleware.Common.Models;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace Middleware.TaskPlanner.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
     public class PlanController : ControllerBase
     {
+       
+
         [HttpPost] //http get request
         [ProducesResponseType(typeof(TaskModel), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<TaskModel>> GetPlan([FromBody]TaskPlannerInputModel inputModel)
@@ -15,5 +18,7 @@ namespace Middleware.TaskPlanner.Controllers
             var plan =  new TaskModel();  
             return Ok(plan);
         }
+
+      
     }
 }
