adbf0ef1f8bb7d468cb9bfd9770cded78a920d0a
Bartosz Bratuś <bartosz.bratus@study.beds.ac.uk>
2022-03-10 15:40:05 +0000

    Move LuaQueries to RedisInterface

--- src/RedisInterface/LuaQueries/GetKeys.lua
+++ src/RedisInterface/LuaQueries/GetKeys.lua
@@ -1,6 +1,6 @@
 -- This LUA script will query for all the policies inside redis with the attribute IsActive set to True. It will return to IsActive parameter and the Policy_Id.
 
-redis.call('select', '6')
+redis.call('select', '3')
 
 local KeysArray = {};
 local matches = redis.call('KEYS', '*')
