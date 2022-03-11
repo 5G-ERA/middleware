5920c0963874e34160352c1d4eb71c3d94d8bc71
Adrian Lendinez <adrian.lendinez@outlook.com>
2022-03-11 16:36:12 +0100

    added corrected IsActivePolicies lua script

--- src/RedisInterface/LuaQueries/GetActivePolicies.lua
+++ src/RedisInterface/LuaQueries/GetActivePolicies.lua
@@ -4,10 +4,17 @@ redis.call('select', '3')
 
 local ActivePoliciesArray = {};
 local matches = redis.call('KEYS', '*')
+local index = 0
 
 for value,key in ipairs(matches) do
-    local val = redis.call('json.get', key, 'IsActive', 'Policy_Id')
-    table.insert(ActivePoliciesArray, val);
+
+    local PolicyId = redis.call('json.get', key, 'Policy_Id')
+         local IsActive = redis.call('json.get', key, 'IsActive')
+         local PolicyAllObject = redis.call('json.get',key)
+         if (IsActive =="true") then table.insert(ActivePoliciesArray,PolicyAllObject) end
+
+         index = index +1
 end
 
 return ActivePoliciesArray
+
