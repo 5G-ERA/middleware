6d7da4dba16eb31927e1fd3b49f4241ff35f9fbb
adrianLIrobotics <adrian.lendinez@outlook.com>
2022-03-17 11:44:41 +0100

    Created basic template for Resource planner c# & actionPlanner

---
+++ src/ResourcePlanner/ResourcePlanner.cs
@@ -0,0 +1,24 @@
+﻿namespace Middleware.ResourcePlanner
+{
+    public class ResourcePlanner
+    {
+    }
+
+    // actionPlanner will give resource planner the actionSequence. 
+
+    // Get action sequence 
+
+    // query redis for best placement. 
+
+    // call redis API
+    //get active policies: get closet machine.
+    action1, action2, actio3 --> 7,1,1,
+
+    variableFreeActors = HttpsRedirectionServicesExtensions.API.graphQuery
+    [edge1,cloud1]
+            // use redis grpah querys that we have to get the data of the script.
+            // script to perform logic of best decision.
+
+
+    //
+}
---
+++ src/TaskPlanner/ActionPlanner.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Middleware.Common.Models;
+
+namespace Middleware.TaskPlanner
+{
+    public class ActionPlanner
+    {
+        //**Get all the data neccesary to plan**
+        //============================================
+        // --> get task_id
+        // --> get task_priority
+        // --> Get all questions and answers
+        // --> get list of all task_ids registered in the system
+        //--> get robot model
+        //--> get LocomotionSystem
+        //--> get "ArticulationAvailable": "false",
+        //--> get "NumberOfArticulation": 0,
+        //-->  "ArticulationDof": [],
+        //-->  "Sensors": [ "lidar", "camera", "IMU" ],
+        //============================================
+
+
+        public Guid ActionPlanId { get; set; }
+        public List<ActionModel> ActionSequence { get; set; }
+        public TimeOnly TimeOnly { get; set; }  
+        
+        public ActionPlanner (Guid ActionPlanningId, List<ActionModel> SequenceActions, TimeOnly CurrentTime)
+        {
+            ActionPlanId = ActionPlanningId;
+            ActionSequence = SequenceActions;
+            TimeOnly = CurrentTime;
+
+        }
+    }
+}
+
+//first get task and robot parameters from redis table Robot and task
+
+// select to use manual or AI model.
+
+
+
+// taskid -1
+
+//
