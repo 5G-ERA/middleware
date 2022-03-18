da14bc42e2a8f582755baca549345f66f916b97c
Bartosz Bratuś <bartosz.bratus@study.beds.ac.uk>
2022-03-18 16:18:16 +0000

    Add Api clients builder and test the connectivity

--- src/Orchestrator/Program.cs
+++ src/Orchestrator/Program.cs
@@ -1,3 +1,8 @@
+using System.Net;
+using Middleware.Orchestrator.ApiReference;
+using Middleware.Orchestrator.Config;
+using Middleware.Orchestrator.RedisInterface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.ConfigureAutoMapper();
+builder.Services.AddHttpClient(AppConfig.RedisApiClientName);
+builder.Services.AddHttpClient(AppConfig.RedisApiClientName);
+builder.Services.AddScoped<IApiClientBuilder, ApiClientBuilder>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +23,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+ServicePointManager
+        .ServerCertificateValidationCallback +=
+    (sender, cert, chain, sslPolicyErrors) => true;
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
