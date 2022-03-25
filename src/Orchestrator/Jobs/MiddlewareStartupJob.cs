bcdb6a65c23920d4ca8733aab065631bcb15557d
Bartosz Bratuś <bartosz.bratus@study.beds.ac.uk>
2022-03-25 12:15:35 +0100

    Added Quartz job definition to startup the Middleware

---
+++ src/Orchestrator/Jobs/MiddlewareStartupJob.cs
@@ -0,0 +1,19 @@
+﻿using Quartz;
+
+namespace Middleware.Orchestrator.Jobs
+{
+    public class MiddlewareStartupJob : IJob
+    {
+        private readonly ILogger<MiddlewareStartupJob> _logger;
+
+        public MiddlewareStartupJob(ILogger<MiddlewareStartupJob> logger)
+        {
+            _logger = logger;
+        }
+        public async Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Executed");
+            //throw new NotImplementedException();
+        }
+    }
+}
--- src/Orchestrator/Program.cs
+++ src/Orchestrator/Program.cs
@@ -1,7 +1,7 @@
-using System.Net;
 using Middleware.Orchestrator.ApiReference;
 using Middleware.Orchestrator.Config;
-using Middleware.Orchestrator.RedisInterface;
+using Middleware.Orchestrator.Jobs;
+using Quartz;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +17,25 @@ builder.Services.AddHttpClient(AppConfig.RedisApiClientName);
 builder.Services.AddHttpClient(AppConfig.RedisApiClientName);
 builder.Services.AddScoped<IApiClientBuilder, ApiClientBuilder>();
 builder.Services.AddHttpClient("healthCheckClient");
+
+// quartz 
+builder.Services.AddQuartz(q =>
+{
+    q.UseMicrosoftDependencyInjectionJobFactory();
+
+    q.ScheduleJob<MiddlewareStartupJob>(trg => trg
+        .WithIdentity("Middleware startup Job")
+        .WithDescription("Job that starts the whole Middleware system")
+        .StartNow()
+    );
+});
+builder.Services.AddQuartzHostedService(opt =>
+{
+    opt.WaitForJobsToComplete = true;
+});
+
+builder.Services.AddTransient<MiddlewareStartupJob>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
