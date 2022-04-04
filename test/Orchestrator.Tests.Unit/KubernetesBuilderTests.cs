ff5788142e506819219ed205082717ba8d49f462
Bartosz Bratuś <bartosz.bratus@study.beds.ac.uk>
2022-04-04 14:30:30 +0100

    Configure additional tests

--- test/Orchestrator.Tests.Unit/KubernetesBuilderTests.cs
+++ test/Orchestrator.Tests.Unit/KubernetesBuilderTests.cs
@@ -1,7 +1,10 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
+using k8s;
 using Microsoft.Extensions.Logging;
 using Middleware.Common;
 using Middleware.Orchestrator.Deployment;
+using Middleware.Orchestrator.Exceptions;
 using NSubstitute;
 using Xunit;
 
@@ -19,7 +22,7 @@ public class KubernetesBuilderTests
     }
 
     [Fact]
-    public void CreateKubernetesClient_ShouldCreateClient_When_EnvironmentVarsAreSet()
+    public void CreateKubernetesClient_ShouldCreateClient_WhenEnvironmentVarsAreSet()
     {
         // arrange
         _env.GetEnvVariable("KUBERNETES_SERVICE_HOST").Returns("192.168.0.1");
@@ -28,5 +31,16 @@ public class KubernetesBuilderTests
         var client = _sut.CreateKubernetesClient();
         // assess
         client.Should().NotBeNull();
+    }
+    [Fact]
+    public void CreateKubernetesClient_ShouldThrowException_WhenEnvironmentVarsAreNotSet()
+    {
+        // arrange
+        _env.GetEnvVariable("KUBERNETES_SERVICE_HOST").Returns("");
+        _env.GetEnvVariable("KUBERNETES_SERVICE_PORT").Returns("");
+        // act
+        Func<IKubernetes> act = () => _sut.CreateKubernetesClient();
+        // assess
+        act.Should().Throw<NotInK8sEnvironmentException>().WithMessage("The environment is not a Kubernetes environment, cannot instantiate the Middleware");
     }
 }
