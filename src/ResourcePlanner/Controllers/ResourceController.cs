11485ce01f3e89d958dbea997efa3db9240af3e1
Bartosz Bratuś <bartosz.bratus@study.beds.ac.uk>
2022-05-12 15:50:40 +0100

    Updated API error handling in Resource Planner

--- src/ResourcePlanner/Controllers/ResourceController.cs
+++ src/ResourcePlanner/Controllers/ResourceController.cs
@@ -1,6 +1,7 @@
-﻿using Middleware.Common.Models;
+﻿using System.Net;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
+using Middleware.Common.Models;
 
 namespace Middleware.ResourcePlanner.Controllers
 {
@@ -9,22 +10,36 @@ namespace Middleware.ResourcePlanner.Controllers
     public class ResourceController : ControllerBase
     {
         private readonly IResourcePlanner _resourcePlanner;
+        private readonly IMapper _mapper;
 
-
-        public ResourceController(IResourcePlanner resourcePlanner)
+        public ResourceController(IResourcePlanner resourcePlanner, IMapper mapper)
         {
             _resourcePlanner = resourcePlanner;
+            _mapper = mapper;
         }
 
         [HttpPost(Name = "GetResourcePlan")]
         [ProducesResponseType(typeof(TaskModel), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<TaskModel>> GetResource([FromBody] TaskModel inputmodel)
+        [ProducesResponseType(typeof(ApiResponse), (int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(ApiResponse), (int)HttpStatusCode.InternalServerError)]
+        
+        public async Task<ActionResult<TaskModel>> GetResource([FromBody] TaskModel inputModel)
         {
-
-            TaskModel updatedTask =  await _resourcePlanner.Plan(inputmodel);
-
-            
-            return Ok(updatedTask);
+            try
+            {
+                TaskModel updatedTask = await _resourcePlanner.Plan(inputModel);
+                return Ok(updatedTask);
+            }
+            catch (Orchestrator.ApiException<RedisInterface.ApiResponse> apiEx)
+            {
+                return StatusCode(apiEx.StatusCode,_mapper.Map<ApiResponse>(apiEx.Result));
+            }
+            catch (Exception ex)
+            {
+                int statusCode = (int)HttpStatusCode.InternalServerError;
+                return StatusCode(statusCode,
+                    new ApiResponse(statusCode, $"There was an error while collecting the resources: {ex.Message}"));
+            }
         }
     }
 }
