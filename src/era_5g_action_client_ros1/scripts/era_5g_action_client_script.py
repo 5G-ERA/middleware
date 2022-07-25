fa5bcdbeb57154473a9bb4b7574d2664fc9e40d8
Adrian Lendinez <adrian.lendinez@outlook.com>
2022-07-25 11:50:36 +0200

    Update era_5g_action_client_script.py

--- src/era_5g_action_client_ros1/scripts/era_5g_action_client_script.py
+++ src/era_5g_action_client_ros1/scripts/era_5g_action_client_script.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 import actionlib
 
@@ -9,8 +11,10 @@ from std_msgs.msg import String
 #rospy.init_node('era-5g-action-client-script')
 
 
+def feedback_cb(msg):
+    print('Feedback received: '+str(msg))
 
-def talker():
+def goal():
         client = actionlib.SimpleActionClient('goal_5g', goal_5gAction)
         taskId = "11071d4d-d1ae-4e55-8de2-e562c6078277"  # Example of task that the robot wants to execute - make sure the taskid is in redis.
 
@@ -18,14 +22,16 @@ def talker():
         goal.goal_taskid =taskId
         goal.action_reference =0
         #goal = era_5g_action_client_ros1.msg.5gGoal((taskId,0))
+        rospy.loginfo('Waiting for 5g-era-server...')
+        client.wait_for_server()
 
-
-        client.send_goal(goal)
+        client.send_goal(goal,feedback_cb=feedback_cb)
 
 
 if __name__ == '__main__':
     try:
         rospy.init_node('era_5g_action_client_script')
-        talker()
+        rospy.loginfo('Running 5g-era-client...')
+        goal()
     except rospy.ROSInterruptException:
         pass
