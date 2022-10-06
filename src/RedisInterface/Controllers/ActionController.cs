b5f97484c16a332e09cae74af968d670fd33219a
Bartosz Bratuś <bartosz.bratus@gmail.com>
2022-10-06 11:21:21 +0200

    Moved function to actionController, delete unnecesary controller

--- src/RedisInterface/Controllers/ActionController.cs
+++ src/RedisInterface/Controllers/ActionController.cs
@@ -429,7 +429,39 @@ namespace Middleware.RedisInterface.Controllers
                 return StatusCode(statusCode, new ApiResponse(statusCode, $"An error has occurred: {ex.Message}"));
             }
         }
-
+        /// <summary>
+        /// Get action plan given robot Id.
+        /// </summary>
+        /// <returns>List<ActionPlanModel></returns>
+        [HttpGet]
+        [Route("plan/robot/{robotId}", Name = "GetActionPlanByRobotIdAsync")]
+        [ProducesResponseType(typeof(List<ActionPlanModel>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ApiResponse), (int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(ApiResponse), (int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult<List<ActionPlanModel>>> GetActionPlanByRobotIdAsync(Guid robotId)
+        {
+            try
+            {
+                if (robotId == Guid.Empty)
+                {
+                    return BadRequest(new ApiResponse((int)HttpStatusCode.BadRequest, "Robot id has not been specified"));
+                }
+                // Get list of actionPlans from specific robotId.
+                List<ActionPlanModel> actionPlans = await _actionPlanRepository.GetActionPlanModelsAsync(robotId);
+                if (actionPlans == null)
+                {
+                    return NotFound(new ApiResponse((int)HttpStatusCode.NotFound, "Object was not found."));
+                }
+                //List<ActionPlanModel> activePoliciesRecords = actionPlans.Select(p => new ActionPlanModel(p.Id, p.Name, p.Description)).ToList();
+                return Ok(actionPlans);
+            }
+            catch (Exception ex)
+            {
+                int statusCode = (int)HttpStatusCode.InternalServerError;
+                _logger.LogError(ex, "An error occurred:");
+                return StatusCode(statusCode, new ApiResponse(statusCode, $"An error has occurred: {ex.Message}"));
+            }
+        }
         #endregion
 
     }
