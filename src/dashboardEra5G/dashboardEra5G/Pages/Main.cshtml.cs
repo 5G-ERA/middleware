eab2d1678a889d4a7a520901b1942d8903e05bd9
adrianLIrobotics <adrian.lendinez@outlook.com>
2022-12-04 22:04:37 +0100

    Enhanced main and fixes

---
+++ src/dashboardEra5G/dashboardEra5G/Pages/Main.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dashboard5GEra.Pages
+{
+    public class MainModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
