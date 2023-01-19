b4c0ceb89bf04b5a80545e7293f9557a8bad822c
Bartosz Bratuś <bartosz.bratus@study.beds.ac.uk>
2023-01-19 08:38:26 +0100

    Correct orchestrator deployment script

--- k8s/orchestrator/deploy.sh
+++ k8s/orchestrator/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-kubectl apply -f orchestrator_service.yaml -n middleware
-
-kubectl apply -f orchestrator_deployment.yaml -n middleware
+kubectl apply -f orchestrator.yaml -n middleware
 
 
