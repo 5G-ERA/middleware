186eb27ac832b3995e0e260db0666781318a9dd2
Bartosz Bratuś <bartosz.bratus@study.beds.ac.uk>
2023-02-24 13:49:37 +0100

    Index Id property of base Dto type

--- src/Models/Dto/Dto.cs
+++ src/Models/Dto/Dto.cs
@@ -1,9 +1,11 @@
 ﻿using Middleware.Models.Domain;
+using Redis.OM.Modeling;
 
 namespace Middleware.Models.Dto;
 
 public abstract class Dto
 {
+    [Indexed] 
     public abstract string Id { get; set; }
 
     public abstract BaseModel ToModel();
