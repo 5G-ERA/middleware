e15a4b97e0fe1ab93def26c8901146d05199b458
Bartosz Bratuś <bartosz.bratus@study.beds.ac.uk>
2023-02-28 15:46:50 +0100

    Update Policy controller, add policy validation

---
+++ src/RedisInterface/Validation/PolicyRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Middleware.Models.Enums;
+using Middleware.RedisInterface.Contracts.Requests;
+
+namespace Middleware.RedisInterface.Validation;
+
+public class PolicyRequestValidator : AbstractValidator<PolicyRequest>
+{
+    public PolicyRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotNull().NotEmpty();
+        RuleFor(x => x.Description)
+            .NotNull().NotEmpty();
+        RuleFor(x=>x.Type)
+            .NotNull().IsEnumName(typeof(PolicyType), caseSensitive: false)
+            .WithMessage(x =>
+                $"{x.Type} is not a validPolicy Type name. " +
+                $"Valid options are: {string.Join(", ", Enum.GetNames<PolicyType>())}");
+        //TODO: add validation if the policy can be active when it is added or activated
+    }
+}
