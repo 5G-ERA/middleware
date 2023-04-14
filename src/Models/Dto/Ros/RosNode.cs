ca7b664a306f21085fb6952868cf66c6d63f86a1
Bartosz Bratuś <34897359+Artonus@users.noreply.github.com>
2023-04-14 16:15:46 +0200

    Fix display of the robot properties

--- src/Models/Dto/Ros/RosNode.cs
+++ src/Models/Dto/Ros/RosNode.cs
@@ -1,18 +1,30 @@
-﻿using Redis.OM.Modeling;
+﻿using Middleware.Models.Domain;
+using Redis.OM.Modeling;
 
 namespace Middleware.Models.Dto.Ros;
 
 public class RosNode
 {
     [Indexed]
-    public string? Name { get; set; }
+    public string? Name { get; init; }
     [Indexed(JsonPath = "$.Name")]
     [Searchable(JsonPath = "$.Description")]
-    public List<RosTopic> Publications { get; set; } = new();
+    public List<RosTopic> Publications { get; init; } = new();
     [Indexed(JsonPath = "$.Name")]
     [Searchable(JsonPath = "$.Description")]
-    public List<RosTopic> Subscriptions { get; set; } = new();
+    public List<RosTopic> Subscriptions { get; init; } = new();
     [Indexed(JsonPath = "$.Name")]
     [Searchable(JsonPath = "$.Description")]
-    public List<RosService> Services { get; set; } = new();
+    public List<RosService> Services { get; init; } = new();
+
+    public ROSNodeModel ToModel()
+    {
+        return new ROSNodeModel()
+        {
+            Name = this.Name!,
+            Publications = this.Publications.Select(x => x.ToModel()).ToList(),
+            Services = this.Services.Select(x => x.ToModel()).ToList(),
+            Subscriptions = this.Subscriptions.Select(x => x.ToModel()).ToList()
+        };
+    }
 }
--- src/Models/Dto/Ros/RosService.cs
+++ src/Models/Dto/Ros/RosService.cs
@@ -1,7 +1,18 @@
-﻿namespace Middleware.Models.Dto.Ros;
+﻿using Middleware.Models.Domain;
+
+namespace Middleware.Models.Dto.Ros;
 
 public class RosService
 {
-    public string? Name { get; set; }
-    public string? Description { get; set; }
+    public string? Name { get; init; }
+    public string? Description { get; init; }
+
+    public ROSServiceModel ToModel()
+    {
+        return new ROSServiceModel()
+        {
+            Name = this.Name!,
+            Description = this.Description!
+        };
+    }
 }
