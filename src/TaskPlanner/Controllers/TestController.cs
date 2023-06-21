0df86dfa1ab4214f6260d52f34d87fa606574605
radu-popescu <radu.popescu@outlook.com>
2023-06-21 13:55:45 +0100

    wip dynamicroute

---
+++ src/TaskPlanner/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Middleware.TaskPlanner.Controllers;
+
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class TestController : ControllerBase
+{
+
+    [HttpGet]
+    [Route("hello", Name = "Hello")]
+    public IActionResult Hello()
+    {
+        return Ok("hello there");
+    }
+}
