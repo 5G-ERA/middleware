b843b051b05fee2410eff1eb935caeef682359ed
Bartosz Bratuś <bartoszryszard.bratus@beds.ac.uk>
2023-10-13 15:43:22 +0200

    feat: new status endpoints for dashboard

---
+++ src/Orchestrator/Heartbeat/IHeartbeatService.cs
@@ -0,0 +1,11 @@
+﻿using Middleware.Models.Domain;
+
+namespace Middleware.Orchestrator.Heartbeat;
+
+public interface IHeartbeatService
+{
+    Task<NetAppStatusModel> GetNetAppStatusByIdAsync(Guid id, bool generateFakeData = false);
+    Task<RobotStatusModel> GetRobotStatusByIdAsync(Guid id, bool generateFakeData = false);
+    Task<IReadOnlyList<NetAppStatusModel>> GetAllAppStatusesAsync(bool generateFakeData = false);
+    Task<IReadOnlyList<RobotStatusModel>> GetAllRobotStatusesAsync(bool generateFakeData = false);
+}
