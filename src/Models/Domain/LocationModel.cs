77ca82a802fa22329eeb5b12cfbe1465a49482f4
Bartosz Bratuś <bartoszryszard.bratus@beds.ac.uk>
2023-11-08 10:05:28 +0100

    feat: added locationDto and LocationModel

---
+++ src/Models/Domain/LocationModel.cs
@@ -0,0 +1,80 @@
+﻿using Middleware.Models.Domain.Contracts;
+using Middleware.Models.Dto;
+using Middleware.Models.Enums;
+
+namespace Middleware.Models.Domain;
+
+public class LocationModel : BaseModel, ILocation
+{
+    public string Status { get; set; } = default!;
+    public int? NumberOfCores { get; set; }
+    public long? DiskStorage { get; set; }
+    public long? VirtualRam { get; set; }
+    public int? Cpu { get; set; }
+    public long? Ram { get; set; }
+    public string? MacAddress { get; set; }
+    public DateTime LastUpdatedTime { get; set; }
+    public bool IsOnline { get; set; }
+    public override Guid Id { get; set; } = Guid.NewGuid();
+    public override string Name { get; set; } = default!;
+    public LocationType Type { get; set; } = LocationType.Cloud;
+    public string Organization { get; set; } = default!;
+    public Uri Address { get; set; } = default!;
+
+    /// <inheritdoc />
+    public BaseModel ToBaseLocation()
+    {
+        return this;
+    }
+
+    /// <inheritdoc />
+    public Location ToLocation()
+    {
+        var that = this;
+        return new()
+        {
+            Address = that.Address,
+            Name = that.Name,
+            Id = that.Id,
+            Organization = that.Organization,
+            Type = LocationType.Cloud
+        };
+    }
+
+    /// <inheritdoc />
+    public string GetNetAppAddress(string netAppName)
+    {
+        return GetNetAppAddress(netAppName, Address);
+    }
+
+    /// <inheritdoc />
+    public string GetNetAppStatusReportAddress()
+    {
+        return GetNetAppReportAddress(Address);
+    }
+
+    public override Dto.Dto ToDto()
+    {
+        var domain = this;
+        return new LocationDto
+        {
+            Id = domain.Id.ToString(),
+            Name = domain.Name,
+            Type = Type.ToString(),
+            Organization = domain.Organization,
+            Status = domain.Status,
+            Address = domain.Address,
+            HardwareSpec = new()
+            {
+                Cpu = domain.Cpu,
+                StorageDisk = domain.DiskStorage,
+                VirtualRam = domain.VirtualRam,
+                Ram = domain.Ram,
+                NumberCores = domain.NumberOfCores
+            },
+            MacAddress = domain.MacAddress,
+            LastUpdatedTime = domain.LastUpdatedTime == default ? DateTimeOffset.Now : domain.LastUpdatedTime,
+            IsOnline = domain.IsOnline
+        };
+    }
+}
---
+++ src/Models/Dto/LocationDto.cs
@@ -0,0 +1,64 @@
+﻿using Middleware.Models.Domain;
+using Middleware.Models.Dto.Hardware;
+using Redis.OM.Modeling;
+
+namespace Middleware.Models.Dto;
+
+[Document(IndexName = "location-idx", StorageType = StorageType.Json, Prefixes = new[] { Prefix })]
+public class LocationDto : Dto
+{
+    public const string Prefix = "Location";
+
+    [Indexed]
+    [RedisIdField]
+    public override string Id { get; set; }
+
+    [Indexed]
+    public string? Name { get; set; }
+
+    [Indexed]
+    public string Type { get; set; } = default!;
+
+    [Indexed]
+    public string Organization { get; set; } = default!;
+
+    [Indexed]
+    public string? Status { get; set; }
+
+    [Indexed]
+    public Uri Address { get; set; }
+
+    [Indexed]
+    public HardwareSpec HardwareSpec { get; set; }
+
+    [Indexed]
+    public string? MacAddress { get; set; }
+
+    [Indexed(Sortable = true)]
+    public DateTimeOffset LastUpdatedTime { get; set; }
+
+    [Indexed]
+    public bool IsOnline { get; set; }
+
+    /// <inheritdoc />
+    public override BaseModel ToModel()
+    {
+        var dto = this;
+        return new LocationModel
+        {
+            Id = Guid.Parse(dto.Id!.Replace(Prefix, "")),
+            Name = dto.Name,
+            Organization = dto.Organization,
+            Status = dto.Status,
+            Address = dto.Address,
+            NumberOfCores = dto.HardwareSpec.NumberCores,
+            DiskStorage = dto.HardwareSpec.StorageDisk,
+            VirtualRam = dto.HardwareSpec.VirtualRam,
+            Cpu = dto.HardwareSpec.Cpu,
+            Ram = dto.HardwareSpec.Ram,
+            MacAddress = dto.MacAddress,
+            LastUpdatedTime = dto.LastUpdatedTime.DateTime,
+            IsOnline = dto.IsOnline
+        };
+    }
+}
