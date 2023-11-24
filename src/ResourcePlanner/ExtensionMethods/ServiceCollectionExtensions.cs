a13eecf194623318d46a6029378755d97504d7fa
Bartosz Bratuś <bartoszRyszard.bratus@beds.ac.uk>
2023-11-24 15:49:06 +0100

    fix: rework connectivity method between middleware services (#231)
    * feat: create publisher for resourcePlan request
    
    * feat: final structure for sending event with the response
    
    * fix: centralApi starting slowly in dev environment
    
    * fix: change IndexCreationService to BackgroundService for long running task
    
    * fix: compilation errors in Resource and Task planners
    
    * feat: working resource planner queue
    
    * feat: pass error message from ResourcePlanner to TaskPlanner

---
+++ src/ResourcePlanner/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using MassTransit;
+using Middleware.Common.Config;
+using Middleware.Common.Helpers;
+using Middleware.ResourcePlanner.Handlers;
+
+namespace Middleware.ResourcePlanner.ExtensionMethods;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection RegisterRabbitMqConsumers(this IServiceCollection services,
+        RabbitMqConfig mqConfig, MiddlewareConfig mwConfig)
+    {
+        services.AddMassTransit(x =>
+        {
+            services.AddScoped<RequestResourcePlanHandler>();
+            x.AddConsumer<RequestResourcePlanHandler>()
+                .Endpoint(e => e.Name = QueueHelpers.ConstructResourcePlanningServiceQueueName(mwConfig.Organization,
+                    mwConfig.InstanceName));
+            ;
+
+            x.UsingRabbitMq((busRegistrationContext, mqBusFactoryConfigurator) =>
+            {
+                mqBusFactoryConfigurator.Host(mqConfig.Address, "/", hostConfig =>
+                {
+                    hostConfig.Username(mqConfig.User);
+                    hostConfig.Password(mqConfig.Pass);
+                });
+
+                mqBusFactoryConfigurator.ReceiveEndpoint(
+                    QueueHelpers.ConstructResourcePlanningServiceQueueName(mwConfig.Organization,
+                        mwConfig.InstanceName),
+                    ec =>
+                    {
+                        ec.ConfigureConsumeTopology = false;
+                        ec.ConfigureConsumer<RequestResourcePlanHandler>(busRegistrationContext);
+                    });
+
+
+                mqBusFactoryConfigurator.ConfigureEndpoints(busRegistrationContext);
+            });
+        });
+        return services;
+    }
+}
