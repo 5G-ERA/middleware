984ccb4a7bac38ec4b3b62adec36e8fc683ede39
Bartosz Bratuś <34897359+Artonus@users.noreply.github.com>
2024-01-26 10:59:34 +0100

    build: allow internals to be visible from test project

---
+++ src/ResourcePlanner/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//[assembly: AssemblyTitle("Middleware.ResourcePlanner")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("Middleware.ResourcePlanner")]
+//[assembly: AssemblyCopyright("Copyright ©  2024")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("ResourcePlanner.Tests.Unit")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("caad4bde-55f9-4cca-a72b-62180881470d")]
