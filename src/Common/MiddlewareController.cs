b10bc00ea2984b051d19ed84f78a388a4d1548e3
Bartosz Bratuś <34897359+Artonus@users.noreply.github.com>
2024-02-19 13:43:47 +0100

    refactor(gateway): use middleware controller for consistent error messages

--- src/Common/MiddlewareController.cs
+++ src/Common/MiddlewareController.cs
@@ -9,6 +9,7 @@ public class MiddlewareController : Controller
     {
         return GetResponse(code, null);
     }
+
     protected IActionResult ErrorMessageResponse(HttpStatusCode code, string property, string message)
     {
         var error = new ValidationProblemDetails()
@@ -40,15 +41,17 @@ public class MiddlewareController : Controller
                 retVal.Errors.Add(error);
             }
         }
+
         return GetResponse(code, retVal);
     }
-    
+
     private IActionResult GetResponse(HttpStatusCode code, object? data)
     {
         var resp = code switch
         {
-            HttpStatusCode.NotFound => NotFound(data),
             HttpStatusCode.BadRequest => BadRequest(data),
+            HttpStatusCode.Unauthorized => Unauthorized(data),
+            HttpStatusCode.NotFound => NotFound(data),
             HttpStatusCode.InternalServerError => StatusCode(500, data),
             _ => throw new ArgumentOutOfRangeException(nameof(code), code, null)
         };
