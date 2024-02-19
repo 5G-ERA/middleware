bf7671fd179c364f8e47280feba2565e717bc553
Bartosz Bratuś <34897359+Artonus@users.noreply.github.com>
2024-02-19 12:36:26 +0100

    feat: add middleware controller

---
+++ src/Common/MiddlewareController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Middleware.Common;
+
+public class MiddlewareController : Controller
+{
+    protected IActionResult ErrorMessageResponse(HttpStatusCode code, string property, string message)
+    {
+        var error = new ValidationProblemDetails()
+        {
+            Status = (int)code,
+            Extensions =
+            {
+                ["traceId"] = HttpContext.TraceIdentifier
+            }
+        };
+        error.Errors.Add(new(property, new[] { message }));
+        var resp = code switch
+        {
+            HttpStatusCode.NotFound => NotFound(error),
+            HttpStatusCode.BadRequest => BadRequest(error),
+            HttpStatusCode.InternalServerError => StatusCode(500, error),
+            _ => throw new ArgumentOutOfRangeException(nameof(code), code, null)
+        };
+        return resp;
+    }
+}
