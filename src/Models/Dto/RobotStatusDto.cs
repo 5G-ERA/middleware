d0fbf7c35eee171ae1098817b6e9ca6a1c8ce29c
Bartosz Bratuś <34897359+Artonus@users.noreply.github.com>
2024-02-20 16:07:19 +0100

    fix: retrieving heartbeat statuses when data is not compleate; return only latest records

--- src/Models/Dto/RobotStatusDto.cs
+++ src/Models/Dto/RobotStatusDto.cs
@@ -40,9 +40,9 @@ public class RobotStatusDto : InfluxDto
         }
 
         // static properties of all objects
-        var objName = fluxTables[0].Records[0].GetValueByKey(ObjectType).ToString();
-        var objId = fluxTables[0].Records[0].GetValueByKey("Id").ToString();
-        var objTimestamp = fluxTables[0].Records[0].GetTimeInDateTime();
+        var objName = fluxTables[0].Records[0].GetValueByKey(ObjectType)?.ToString();
+        var objId = fluxTables[0].Records[0].GetValueByKey("Id")?.ToString();
+        var objTimestamp = fluxTables[0].Records[0]?.GetTimeInDateTime();
 
         if (objName == null || objId == null || objTimestamp == null)
         {
