f3df4d37e22733ff8ddad6c53e0b04cab4bb0ba6
Bartosz Bratuś <artonus1@gmail.com>
2024-02-29 13:43:11 +0100

    refactor: skip assigning qos properties when they are null

--- src/Orchestrator/Deployment/RosCommunication/Structures/QosContainer.cs
+++ src/Orchestrator/Deployment/RosCommunication/Structures/QosContainer.cs
@@ -7,22 +7,39 @@ namespace Middleware.Orchestrator.Deployment.RosCommunication.Structures;
 internal class QosContainer
 {
     [JsonPropertyName("preset")]
+    [CanBeNull]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string Preset { get; set; } = default!;
     [JsonPropertyName("history")]
+    [CanBeNull]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string History { get; set; } = default!;
     [JsonPropertyName("depth")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public int? Depth { get; set; } = default!;
     [JsonPropertyName("reliability")]
+    [CanBeNull]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string Reliability { get; set; } = default!;
     [JsonPropertyName("durability")]
+    [CanBeNull]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string Durability { get; set; } = default!;
     [JsonPropertyName("deadline")]
+    [CanBeNull]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string Deadline { get; set; } = default!;
     [JsonPropertyName("lifespan")]
+    [CanBeNull]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string Lifespan { get; set; } = default!;
     [JsonPropertyName("liveliness")]
+    [CanBeNull]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string Liveliness { get; set; } = default!;
     [JsonPropertyName("lease")]
+    [CanBeNull]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string Lease { get; set; } = default!;
 
     [CanBeNull]
--- test/Orchestrator.Tests.Unit/Deployment/Ros2ConnectionBuilderTests.cs
+++ test/Orchestrator.Tests.Unit/Deployment/Ros2ConnectionBuilderTests.cs
@@ -158,8 +158,7 @@ public class Ros2ConnectionBuilderTests
         var depl = K8SBuilder.CreateExampleDeployment();
         var sut = new Ros2ConnectionBuilder(distro, cfg);
         var serviceStringWithQos = "[{\"name\":\"/test_srvs\",\"type\":\"/std_srvs/SetBool\"," +
-                "\"qos\":{\"preset\":\"string\",\"history\":\"string\",\"depth\":12,\"reliability\":null," +
-                "\"durability\":null,\"deadline\":null,\"lifespan\":null,\"liveliness\":null,\"lease\":\"string\"}}]";
+                "\"qos\":{\"preset\":\"string\",\"history\":\"string\",\"depth\":12,\"lease\":\"string\"}}]";
         var topics = new List<RosTopicModel>()
         {
             new()
