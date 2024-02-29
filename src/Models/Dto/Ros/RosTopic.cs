c5bf34af31aa722e7dcde03177a63d8ed7ca81b3
Bartosz Bratuś <artonus1@gmail.com>
2024-02-29 10:07:38 +0100

    fix: make Compression in Ros topics optional

--- src/Models/Dto/Ros/RosTopic.cs
+++ src/Models/Dto/Ros/RosTopic.cs
@@ -8,7 +8,7 @@ public class RosTopic
     public string? Type { get; set; }
     public string? Description { get; set; }
     public bool Enabled { get; set; }
-    public string Compression { get; set; } = "none";
+    public string? Compression { get; set; }
 
     public QosDto? Qos { get; set; }
 
