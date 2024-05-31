5e2a3de028b0c3d4c93336055de544ed0ee36fe4
Bartosz Bratuś <bartoszRyszard.bratus@beds.ac.uk>
2024-05-31 12:30:37 +0200

    fix: remove unnecesary json property mappings

--- src/Models/Domain/Ros/RosTransformsModel.cs
+++ src/Models/Domain/Ros/RosTransformsModel.cs
@@ -5,19 +5,14 @@ namespace Middleware.Models.Domain.Ros;
 
 public class RosTransformsModel
 {
-    [JsonPropertyName("source_frame")]
     public string SourceFrame { get; set; } = default!;
 
-    [JsonPropertyName("target_frame")]
     public string TargetFrame { get; set; } = default!;
 
-    [JsonPropertyName("angular_thres")]
     public double AngularThres { get; set; }
 
-    [JsonPropertyName("trans_thres")]
     public double TransThres { get; set; }
 
-    [JsonPropertyName("max_publish_period")]
     public double MaxPublishPeriod { get; set; }
 
     public RosTransforms ToDto()
